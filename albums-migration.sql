82b751a427055ab9c3376605d03eaa07bb9ca230
Kaylee Knox <kjknoxx90@gmail.com>
2023-10-23 12:23:51 -0500

    Edits to script.

--- albums-migration.sql
+++ albums-migration.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS albums;
-CREATE TABLE albums (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(100) NOT NULL,
-    title VARCHAR(150) NOT NULL,
-    release_date DATE NOT NULL,
-    sales INT NOT NULL,
-    genre VARCHAR(20) NOT NULL,
-                    PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS `albums`;
+CREATE TABLE `albums` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `artist` VARCHAR(100) NOT NULL,
+    `title` VARCHAR(150) NOT NULL,
+    `release_date` INT NOT NULL,
+    `sales` DECIMAL(3,1) NOT NULL,
+    `genre` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`id`)
 );
